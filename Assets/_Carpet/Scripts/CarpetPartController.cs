0de646a58ccdff8bb658b23e0200c8fef0cac0bc
Middle Shell <denob311302@gmail.com>
2024-05-07 17:15:15 +0300

    SomeTasks

--- Assets/_Carpet/Scripts/CarpetPartController.cs
+++ Assets/_Carpet/Scripts/CarpetPartController.cs
@@ -6,6 +6,8 @@ public class CarpetPartController : KeyHoleController
 {
     [SerializeField] private SpriteRenderer partRenderer;
     [SerializeField] private int layer = 12;
+    [SerializeField] private Sprite carpetSprite;
+    [SerializeField] private Material standardMaterial;
 
     protected override Renderer GetRenderer()
     {
@@ -14,13 +16,19 @@ public class CarpetPartController : KeyHoleController
     
     protected override void SetUpPart(GameObject other)
     {
-        other.GetComponent<Bounce>().enabled = false;
+        partRenderer.sprite = carpetSprite;
+        partRenderer.material = standardMaterial;
+        var partRendererColor = partRenderer.color;
+        partRendererColor.a = 1;
+        partRenderer.color = partRendererColor;
+        /*other.GetComponent<Bounce>().enabled = false;
         other.GetComponent<Rigidbody>().velocity = Vector2.zero;
         other.GetComponent<Rigidbody>().freezeRotation = true;
         other.gameObject.transform.localPosition = Vector3.zero;
         other.gameObject.transform.localRotation = Quaternion.Euler(0,0,0);
         other.layer = layer;
-        other.GetComponent<Rigidbody>().isKinematic = true;
-        Destroy(this.gameObject);
+        other.GetComponent<Rigidbody>().isKinematic = true;*/
+        Destroy(other.gameObject);
+        this.enabled = false;
     }
 }
