ab884280763b3db13e4cfb0dc61002e266ea6dd5
Dvvan696 <D.vvaann@gmail.com>
2024-05-13 20:13:06 +0300

    fade

--- Assets/_Carpet/Scripts/TripController.cs
+++ Assets/_Carpet/Scripts/TripController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,11 +13,60 @@ public class TripController : MonoBehaviour
     
     [SerializeField] private GameObject player;
 
+   
+    Coroutine m_FadeCoroutine;
+    [SerializeField] private GameObject FadeMat;
+    [SerializeField] private GameObject Tip;
+    [SerializeField] private float speed;
+
+
+
     public void GoToTrip()
     {
         QuestController.instance.NextStep();
         ARRoom.SetActive(true);
         player.transform.position = teleportPosition.position;
         normalRoom.SetActive(false);
+        
+        if (m_FadeCoroutine != null)
+            StopCoroutine(m_FadeCoroutine);
+
+        m_FadeCoroutine =StartCoroutine(Fade());
+    }
+    
+    
+    public IEnumerator Fade()
+    {
+        Renderer rend = FadeMat.transform.GetComponent<Renderer>();
+        float alphaValue = rend.material.GetFloat("_Alpha");
+
+        while (rend.material.GetFloat("_Alpha") < 1f)
+        {
+            alphaValue += Time.deltaTime / speed;
+            rend.material.SetFloat("_Alpha", alphaValue);
+            yield return null;
+        }
+        rend.material.SetFloat("_Alpha", 1f);
+
+        yield return new WaitForSeconds(1.5f);
+        
+        while (rend.material.GetFloat("_Alpha") > 0f)
+            {
+                alphaValue -= Time.deltaTime / speed;
+                rend.material.SetFloat("_Alpha", alphaValue);
+                yield return null;
+            }
+            rend.material.SetFloat("_Alpha", 0f);
+            
+        
+            
+        Tip.SetActive(true);
+            
+        yield return new WaitForSeconds(15f);
+            
+        Tip.SetActive(false);
+        
+           
+        
     }
 }
